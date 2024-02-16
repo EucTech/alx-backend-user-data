6e6c58a9549f9c938e76e527eb8374efd15a86e1
EucTech <euccreative@gmail.com>
2024-02-16 14:15:06 +0100

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,14 +38,14 @@ def before_check():
                       '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    elif auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
         abort(401)
-    if auth.session_cookie(request) is None:
-        abort(401)
-    current_user = auth.current_user(request)
-    if current_user is None:
+    elif auth.authorization_header(request) and auth.session_cookie(request):
+        return None, 401
+    elif auth.current_user(request) is None:
         abort(403)
-    request.current_user = current_user
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
