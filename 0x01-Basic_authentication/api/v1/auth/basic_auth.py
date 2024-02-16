ff148dfa7f1e8399d476984c0c333b0e0a4c5d5b
EucTech <euccreative@gmail.com>
2024-02-16 11:16:20 +0100

    updated

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,9 +44,8 @@ class BasicAuth(Auth):
         except Exception as e:
             return None
 
-     def extract_user_credentials(self,
-                                 decoded_base64_authorization_header:
-                                 str) -> (str, str):
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
         """ extract user credentials function """
         if decoded_base64_authorization_header is None:
             return (None, None)
