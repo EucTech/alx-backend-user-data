0694e46646d5a1302815e62504ce308daa09c35e
EucTech <euccreative@gmail.com>
2024-02-16 10:55:39 +0100

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -29,3 +29,37 @@ class BasicAuth(Auth):
         else:
             parts = authorization_header.split(' ')
             return parts[1]
+
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                               str) -> str:
+    """ decode base64 function """
+    if base64_authorization_header is None:
+        return None
+    if not isinstance(base64_authorization_header, str):
+        return None
+    try:
+        decoded = base64.b64decode(base64_authorization_header)
+        return decoded.decode('utf-8')
+    except Exception as e:
+        return None
+
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """user object function"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for u in users:
+                if u.is_valid_password(user_pwd):
+                    return u
+            return None
+        except Exception:
+            return None
